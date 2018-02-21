4bc2394204dc0d376c6194b88b526856797df5a9
Wolfy Hsu <wolfy_hsu@trend.com.tw>
2018-02-22 02:12:31 +0800

    Numpy & Matplotlib test

--- test/testrun.py
+++ test/testrun.py
@@ -5,4 +5,23 @@ print("x:\n{}".format(x))
 
 from scipy import sparse
 eye = np.eye(4)
+print("\n")
 print("eye:\n{}".format(eye))
+
+sparse_matrix = sparse.csr_matrix(eye)
+print("\n")
+print("sparse CSR matrix:\n{}". format(sparse_matrix))
+
+y = np.array([[2, 0, 0], [0, 3, 0], [0, 1, 4]])
+sparse_matrix2 = sparse.csr_matrix(y)
+print("\n")
+print("sparse CSR matrix:\n{}". format(sparse_matrix2))
+
+import matplotlib.pyplot as plt
+
+x = np.linspace(-10, 10, 100)
+
+y = np.sin(x)
+
+plt.plot(x, y, marker="x")
+plt.show()
