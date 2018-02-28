f574dd1e34817278ab46ff3cdc7fb89cf32b0b05
Wolfy Hsu <wolfy_hsu@trend.com.tw>
2018-02-28 17:19:05 +0800

    Add the second knn_test sample code

---
+++ mltool/knn_test2.py
@@ -0,0 +1,11 @@
+import mglearn
+import matplotlib.pyplot as plt
+
+mglearn.plots.plot_knn_classification(n_neighbors=1)
+plt.show()
+
+mglearn.plots.plot_knn_classification(n_neighbors=3)
+plt.show()
+
+mglearn.plots.plot_knn_classification(n_neighbors=5)
+plt.show()
